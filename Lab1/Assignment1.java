adcd17f61f0e94de2b4ddfa8979a2224e342a2bd
Darius <darius.jurjiu@yahoo.com>
2024-03-02 16:40:22 +0200

    Initial commit

---
+++ Lab1/Assignment1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Assignment1 {
+
+    public static void main(String[] args) {
+        byte[] x = new byte[100];
+        System.out.println("Enter the string");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Press enter to continue");
+        sc.nextLine();
+
+        sc.close();
+
+}
+}
